5d25620658fa72a7b72e438c0317a4a23f0d0f97
Nick Feng <fdj32@163.com>
2022-06-02 03:37:02 -0400

    Routes replace Swith in react-router-dom

--- vault-demo/react-vault-demo/src/components/Main.js
+++ vault-demo/react-vault-demo/src/components/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { RouteConfig } from './config';
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import NavTop from './NavTop';
 import ErrorBoundary from './ErrorBoundary';
 
@@ -10,7 +10,7 @@ export default class Main extends Component {
 
         let routes = RouteConfig.map((item, index) => {
             let links = item.links.map((linkItem, linkIndex) => {
-                return <Route key={index * 100 + linkIndex} path={linkItem.path} component={linkItem.component} />
+                return <Route key={index * 100 + linkIndex} path={linkItem.path} element={linkItem.component} />
             });
             return links;
         });
@@ -18,9 +18,9 @@ export default class Main extends Component {
         return <HashRouter>
             <NavTop />
             <ErrorBoundary>
-                <Switch>
+                <Routes>
                     {routes}
-                </Switch>
+                </Routes>
             </ErrorBoundary>
         </HashRouter>;
     }
