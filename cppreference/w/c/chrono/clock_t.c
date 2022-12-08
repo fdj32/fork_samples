27769b64f17309745e8d963ce5fdb3cecfb13c25
Nick Feng <Nick.Feng@activenetwork.com>
2022-12-08 16:04:18 +0800

    clock_t time_t timespec tm

---
+++ cppreference/w/c/chrono/clock_t.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+ 
+volatile double sink;
+int main (void)
+{
+  clock_t start = clock();
+ 
+  for(size_t i=0; i<3141592; ++i)
+      sink+=sin(i);
+ 
+  clock_t end = clock();
+  double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+ 
+  printf("for loop took %f seconds to execute \n", cpu_time_used);
+}
---
+++ cppreference/w/c/chrono/tm.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <time.h>
+ 
+int main(void)
+{
+    struct tm start = {.tm_year=2022-1900, .tm_mday=1};
+    mktime(&start);
+    printf("%s", asctime(&start));
+}
