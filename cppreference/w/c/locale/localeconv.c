c6822cb81ff4b03ed7a63aefe64115fc7d56597b
Nick Feng <Nick.Feng@activenetwork.com>
2023-01-10 15:41:58 +0800

    https://en.cppreference.com/w/c/locale/localeconv

---
+++ cppreference/w/c/locale/localeconv.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <locale.h>
+ 
+int main(void)
+{
+  setlocale(LC_MONETARY, "en_IN.utf8");
+  struct lconv *lc = localeconv();
+  printf("Local Currency Symbol        : %s\n", lc->currency_symbol);
+  printf("International Currency Symbol: %s\n", lc->int_curr_symbol);
+}
