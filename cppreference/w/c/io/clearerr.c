3266a4790f229bb314b40f3334c4a497effc527c
Nick Feng <Nick.Feng@activenetwork.com>
2023-01-10 15:29:48 +0800

    https://en.cppreference.com/w/c/io/clearerr

---
+++ cppreference/w/c/io/clearerr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <assert.h>
+ 
+int main(void)
+{
+    FILE* tmpf = tmpfile();
+    fputs("cppreference.com\n", tmpf);
+    rewind(tmpf);
+ 
+    for (int ch; (ch = fgetc(tmpf)) != EOF; putchar(ch)) { }
+ 
+    assert(feof(tmpf)); // the loop is expected to terminate by EOF
+    puts("End of file reached");
+ 
+    clearerr(tmpf); // clear EOF
+ 
+    puts(feof(tmpf) ? "EOF indicator set" 
+                    : "EOF indicator cleared");
+}
