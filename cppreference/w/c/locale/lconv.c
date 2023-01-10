f1da2a6d2eefcbd77d3af5bcb2be8ffdb062a5e0
Nick Feng <Nick.Feng@activenetwork.com>
2023-01-10 15:42:35 +0800

    https://en.cppreference.com/w/c/locale/lconv

---
+++ cppreference/w/c/locale/lconv.c
@@ -0,0 +1,9 @@
+#include <locale.h>
+#include <stdio.h>
+ 
+int main(void)
+{
+    setlocale(LC_ALL, "ja_JP.UTF-8");
+    struct lconv *lc = localeconv();
+    printf("Japanese currency symbol: %s(%s)\n", lc->currency_symbol, lc->int_curr_symbol);
+}
