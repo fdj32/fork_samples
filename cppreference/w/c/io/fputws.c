8bba6eeb179b2c3f13bab4ab2d15548f60621026
Nick Feng <Nick.Feng@activenetwork.com>
2023-01-10 15:07:05 +0800

    https://en.cppreference.com/w/c/io/fputws

---
+++ cppreference/w/c/io/fputws.c
@@ -0,0 +1,12 @@
+#include <locale.h>
+#include <stdio.h>
+#include <wchar.h>
+ 
+int main(void)
+{
+    setlocale(LC_ALL, "en_US.utf8");
+    int rc = fputws(L"御休みなさい", stdout);
+ 
+    if (rc == EOF)
+       perror("fputws()"); // POSIX requires that errno is set
+}
