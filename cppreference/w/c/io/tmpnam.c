b44f4df591ee06b3af8d6fcdfcf5d7136dc6c986
Nick Feng <Nick.Feng@activenetwork.com>
2023-01-10 15:36:55 +0800

    https://en.cppreference.com/w/c/io/tmpnam

---
+++ cppreference/w/c/io/tmpnam.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main(void)
+{
+    char* name1 = tmpnam(NULL);
+    printf("temporary file name: %s\n", name1);
+ 
+    char name2[L_tmpnam];
+    if (tmpnam(name2))
+        printf("temporary file name: %s\n", name2);
+}
