2c71d97a4116b3c84867fb72524c7321d9ea7b67
Nick Feng <Nick.Feng@activenetwork.com>
2023-01-11 15:42:11 +0800

    clean build targets on win32

---
+++ cppreference/clean.bat
@@ -0,0 +1,73 @@
+rm ./w/c/algorithm/*.exe
+rm ./w/c/algorithm/*.ilk
+rm ./w/c/algorithm/*.obj
+rm ./w/c/algorithm/*.pdb
+
+rm ./w/c/atomic/*.exe
+rm ./w/c/atomic/*.ilk
+rm ./w/c/atomic/*.obj
+rm ./w/c/atomic/*.pdb
+
+rm ./w/c/chrono/*.exe
+rm ./w/c/chrono/*.ilk
+rm ./w/c/chrono/*.obj
+rm ./w/c/chrono/*.pdb
+
+rm ./w/c/io/*.exe
+rm ./w/c/io/*.ilk
+rm ./w/c/io/*.obj
+rm ./w/c/io/*.pdb
+
+rm ./w/c/io/*.bin
+rm ./w/c/io/*.dat
+rm ./w/c/io/*.txt
+
+rm ./w/c/locale/*.exe
+rm ./w/c/locale/*.ilk
+rm ./w/c/locale/*.obj
+rm ./w/c/locale/*.pdb
+
+rm ./w/c/numeric/complex/*.exe
+rm ./w/c/numeric/complex/*.ilk
+rm ./w/c/numeric/complex/*.obj
+rm ./w/c/numeric/complex/*.pdb
+
+rm ./w/c/numeric/fenv/*.exe
+rm ./w/c/numeric/fenv/*.ilk
+rm ./w/c/numeric/fenv/*.obj
+rm ./w/c/numeric/fenv/*.pdb
+
+rm ./w/c/numeric/math/*.exe
+rm ./w/c/numeric/math/*.ilk
+rm ./w/c/numeric/math/*.obj
+rm ./w/c/numeric/math/*.pdb
+
+rm ./w/c/numeric/random/*.exe
+rm ./w/c/numeric/random/*.ilk
+rm ./w/c/numeric/random/*.obj
+rm ./w/c/numeric/random/*.pdb
+
+rm ./w/c/numeric/tgmath/*.exe
+rm ./w/c/numeric/tgmath/*.ilk
+rm ./w/c/numeric/tgmath/*.obj
+rm ./w/c/numeric/tgmath/*.pdb
+
+rm ./w/c/string/byte/*.exe
+rm ./w/c/string/byte/*.ilk
+rm ./w/c/string/byte/*.obj
+rm ./w/c/string/byte/*.pdb
+
+rm ./w/c/string/multibyte/*.exe
+rm ./w/c/string/multibyte/*.ilk
+rm ./w/c/string/multibyte/*.obj
+rm ./w/c/string/multibyte/*.pdb
+
+rm ./w/c/string/wide/*.exe
+rm ./w/c/string/wide/*.ilk
+rm ./w/c/string/wide/*.obj
+rm ./w/c/string/wide/*.pdb
+
+rm ./w/c/thread/*.exe
+rm ./w/c/thread/*.ilk
+rm ./w/c/thread/*.obj
+rm ./w/c/thread/*.pdb
