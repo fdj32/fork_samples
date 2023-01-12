fb30b062fb5387ebf18cfd2d3485735f741518f9
Nick Feng <Nick.Feng@activenetwork.com>
2023-01-12 11:08:29 +0800

    https://en.cppreference.com/w/cpp/thread/thread/id/operator_ltlt

---
+++ cppreference/w/cpp/thread/thread/id/operator_ltlt.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+using namespace std::chrono;
+ 
+int main()
+{
+    std::thread t1([]{ std::this_thread::sleep_for(256ms); });
+    std::thread t2([]{ std::this_thread::sleep_for(512ms); });
+ 
+    std::clog << t1.get_id() << '\n' << t2.get_id() << '\n';
+ 
+    t1.join(); t2.join();
+}
