ee7de35f97d3941cb47ea94512ec4b4af1bb01cd
Nick Feng <Nick.Feng@activenetwork.com>
2023-01-16 15:41:03 +0800

    automic

---
+++ cppreference/w/cpp/atomic/ATOMIC_FLAG_INIT.cpp
@@ -0,0 +1,10 @@
+#include <atomic>
+ 
+std::atomic_flag static_flag = ATOMIC_FLAG_INIT; // static initialization,
+// guaranteed to be available during dynamic initialization of static objects.
+ 
+int main()
+{
+    std::atomic_flag automatic_flag = ATOMIC_FLAG_INIT; // guaranteed to work
+//    std::atomic_flag another_flag(ATOMIC_FLAG_INIT); // unspecified
+}
