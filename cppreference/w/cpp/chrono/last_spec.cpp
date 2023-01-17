887c2aaf5f3326a01dff6b15ca4b361c93bc2e2e
Nick Feng <Nick.Feng@activenetwork.com>
2023-01-17 22:08:37 +0800

    chrono ok

---
+++ cppreference/w/cpp/chrono/last_spec.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <chrono>
+ 
+int main()
+{
+    using namespace std::chrono;
+ 
+    std::cout << std::boolalpha;
+ 
+    constexpr auto mdl {June/last};
+    std::cout << (mdl == month_day_last(month(6))) << ' ';
+ 
+    constexpr auto ymwdl {year(2023)/December/Tuesday[last]};
+    std::cout << (ymwdl == year_month_weekday_last(
+                           year(2023), month(12), weekday_last(Tuesday))) << '\n';
+}
---
+++ cppreference/w/cpp/chrono/operator_slash.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <chrono>
+using namespace std::chrono;
+ 
+int main()
+{
+    std::cout << std::boolalpha;
+ 
+    constexpr auto ym {2021y/8};
+    std::cout << (ym == year_month(year(2021), August)) << ' ';
+ 
+    constexpr auto md {9/15d};
+    std::cout << (md == month_day(September, day(15))) << ' ';
+ 
+    constexpr auto mdl {October/last};
+    std::cout << (mdl == month_day_last(month(10))) << ' ';
+ 
+    constexpr auto mw {11/Monday[3]};
+    std::cout << (mw == month_weekday(November, Monday[3])) << ' ';
+ 
+    constexpr auto mwdl {December/Sunday[last]};
+    std::cout << (mwdl == month_weekday_last(month(12), weekday_last(Sunday))) << ' ';
+ 
+    // Those three year/month/day orders that people actually use on this planet:
+    constexpr auto ymd {year(2021)/January/day(23)};
+    static_assert(ymd == month{1}/23/2021);
+    static_assert(ymd == day{23}/1/2021);
+    std::cout << (ymd == year_month_day(2021y, month(January), 23d)) << '\n';
+}
