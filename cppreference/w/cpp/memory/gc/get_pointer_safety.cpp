de2fad5060f8023c62f3ecd42cb9ebe537098d6e
Nick Feng <Nick.Feng@activenetwork.com>
2023-02-06 16:49:37 +0800

    g++ -std=c++23 -O2 -Wall -Wextra -pedantic -pthread -pedantic-errors -g -lm w/cpp/memory/gc/get_pointer_safety.cpp -o build/./w/cpp/memory/gc/get_pointer_safety.cpp.exe w/cpp/memory/gc/get_pointer_safety.cpp: In function ‘int main()’: w/cpp/memory/gc/get_pointer_safety.cpp:7:18: error: ‘get_pointer_safety’ is not a member of ‘std’     7 |     switch (std::get_pointer_safety()) {       |                  ^~~~~~~~~~~~~~~~~~ w/cpp/memory/gc/get_pointer_safety.cpp:8:19: error: ‘std::pointer_safety’ has not been declared     8 |         case std::pointer_safety::strict:    std::cout << "strict\n"; break;       |                   ^~~~~~~~~~~~~~ w/cpp/memory/gc/get_pointer_safety.cpp:9:19: error: ‘std::pointer_safety’ has not been declared     9 |         case std::pointer_safety::preferred: std::cout << "preferred\n"; break;       |                   ^~~~~~~~~~~~~~ w/cpp/memory/gc/get_pointer_safety.cpp:10:19: error: ‘std::pointer_safety’ has not been declared    10 |         case std::pointer_safety::relaxed:   std::cout << "relaxed\n"; break;       |                   ^~~~~~~~~~~~~~ make: *** [Makefile:35: build/./w/cpp/memory/gc/get_pointer_safety.cpp.exe] Error 1

--- cppreference/w/cpp/memory/gc/get_pointer_safety.cpp
+++ cppreference/w/cpp/memory/gc/get_pointer_safety.cpp
@@ -4,9 +4,11 @@
 int main()
 {
     std::cout << "Pointer safety: ";
+#ifdef __clang__
     switch (std::get_pointer_safety()) {
         case std::pointer_safety::strict:    std::cout << "strict\n"; break;
         case std::pointer_safety::preferred: std::cout << "preferred\n"; break;
         case std::pointer_safety::relaxed:   std::cout << "relaxed\n"; break;
     }
+#endif
 }
