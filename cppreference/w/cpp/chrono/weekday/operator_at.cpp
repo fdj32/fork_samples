065d67ed8b76b88294c229c589291c5069b1be02
Nick Feng <Nick.Feng@activenetwork.com>
2023-02-07 16:16:51 +0800

    g++ w/cpp/chrono/weekday/operator_at.cpp:13:15: error: no match for ‘operator<<’ (operand types are ‘std::ostream’ {aka ‘std::basic_ostream<char>’} and ‘const std::chrono::year_month_day’)    13 |     std::cout << second_tuesday_in_October_2019 << '\n'

--- cppreference/w/cpp/chrono/weekday/operator_at.cpp
+++ cppreference/w/cpp/chrono/weekday/operator_at.cpp
@@ -9,8 +9,8 @@ int main()
  
     constexpr auto last_tuesday_in_October_2019 =
                    year_month_day { Tuesday[last] / October / 2019y };
-#ifndef __clang__
+/*
     std::cout << second_tuesday_in_October_2019 << '\n'
               << last_tuesday_in_October_2019 << '\n'; 
-#endif
+*/
 }
