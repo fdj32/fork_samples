ea94c043af939181452de58b97875f1417c07a91
Nick Feng <Nick.Feng@activenetwork.com>
2023-02-07 15:39:42 +0800

    g++ -std=c++23 -O2 -Wall -Wextra -pedantic -pthread -pedantic-errors -g -lm -lpthread w/cpp/regex/regex_traits/length.cpp -o build/./w/cpp/regex/regex_traits/length.cpp.exe w/cpp/regex/regex_traits/length.cpp: In function ‘int main()’: w/cpp/regex/regex_traits/length.cpp:6:18: error: use of deleted function ‘std::basic_ostream<char, _Traits>& std::operator<<(basic_ostream<char, _Traits>&, const char8_t*) [with _Traits = char_traits<char>]’     6 |     std::cout << u8"Кошка" << "\n";       |                  ^~~~~~~~~ In file included from /usr/include/c++/12/istream:39,                  from /usr/include/c++/12/sstream:38,                  from /usr/include/c++/12/regex:40,                  from w/cpp/regex/regex_traits/length.cpp:1: /usr/include/c++/12/ostream:647:5: note: declared here   647 |     operator<<(basic_ostream<char, _Traits>&, const char8_t*) = delete;       |     ^~~~~~~~ make: *** [Makefile:35: build/./w/cpp/regex/regex_traits/length.cpp.exe] Error 1

--- cppreference/w/cpp/regex/regex_traits/length.cpp
+++ cppreference/w/cpp/regex/regex_traits/length.cpp
@@ -3,8 +3,8 @@
  
 int main()
 {
-    std::cout << u8"Кошка" << "\n";
-/*
+/*    std::cout << u8"Кошка" << "\n";
+
     std::cout << std::regex_traits<char>::length(u8"Кошка") << '\n'
               << std::regex_traits<wchar_t>::length(L"Кошка") << '\n';
 */
