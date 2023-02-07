2305e6da377ea1ce754eb494415f5f21411745d9
Nick Feng <Nick.Feng@activenetwork.com>
2023-02-07 15:57:20 +0800

    w/cpp/string/wide/iswprint.cpp: In function ‘int main()’: w/cpp/string/wide/iswprint.cpp:11:34: error: use of deleted function ‘std::basic_ostream<char, _Traits>& std::operator<<(basic_ostream<char, _Traits>&, wchar_t) [with _Traits = char_traits<char>]’    11 |               << "iswprint('" << c << "') = "       |                                  ^ In file included from /usr/include/c++/12/iostream:39,                  from w/cpp/string/wide/iswprint.cpp:1: /usr/include/c++/12/ostream:548:5: note: declared here   548 |     operator<<(basic_ostream<char, _Traits>&, wchar_t) = delete;       |     ^~~~~~~~ w/cpp/string/wide/iswprint.cpp:15:34: error: use of deleted function ‘std::basic_ostream<char, _Traits>& std::operator<<(basic_ostream<char, _Traits>&, wchar_t) [with _Traits = char_traits<char>]’    15 |     std::cout << "iswprint('" << c << "') = "       |                                  ^ /usr/include/c++/12/ostream:548:5: note: declared here   548 |     operator<<(basic_ostream<char, _Traits>&, wchar_t) = delete;       |     ^~~~~~~~

--- cppreference/w/cpp/string/wide/iswprint.cpp
+++ cppreference/w/cpp/string/wide/iswprint.cpp
@@ -6,6 +6,8 @@ int main()
 {
     wchar_t c = L'\u2002'; // en-space
     std::setlocale(LC_ALL, "en_US.utf8");
+    // g++ must use before -std=c++20, such as 17, 14, 11, not 20 23
+#ifdef __clang__
     std::cout << std::boolalpha << std::hex << std::showbase
               << "in Unicode locale,\n"
               << "iswprint('" << c << "') = "
@@ -14,4 +16,5 @@ int main()
     c = L'\u0082'; // break permitted
     std::cout << "iswprint('" << c << "') = "
               << (bool)std::iswprint(c) << '\n';
+#endif
 }
