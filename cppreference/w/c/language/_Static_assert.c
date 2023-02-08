565694896e508e6ef2e888a4e056872a32e6e4de
Nick Feng <Nick.Feng@activenetwork.com>
2023-02-08 09:28:17 +0800

    cc -std=c2x -lm -lpthread w/c/language/_Static_assert.c -o build/./w/c/language/_Static_assert.c.exe In file included from w/c/language/_Static_assert.c:1: w/c/language/_Static_assert.c: In function ‘main’: w/c/language/_Static_assert.c:8:5: error: static assertion failed: "this program requires that int is less than char"     8 |     static_assert(sizeof(int) < sizeof(char),       |     ^~~~~~~~~~~~~ make: [Makefile:30: build/./w/c/language/_Static_assert.c.exe] Error 1 (ignored)

--- cppreference/w/c/language/_Static_assert.c
+++ cppreference/w/c/language/_Static_assert.c
@@ -5,6 +5,6 @@ int main(void)
     static_assert(2 + 2 == 4, "Whoa dude!"); // or _Static_assert(...
  
     // This will produce an error at compile time.
-    static_assert(sizeof(int) < sizeof(char),
-                 "this program requires that int is less than char");
+    //static_assert(sizeof(int) < sizeof(char),
+    //             "this program requires that int is less than char");
 }
