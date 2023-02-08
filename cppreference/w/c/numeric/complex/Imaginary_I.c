94dbf4c8b33b9e3fd610d3fd83f72dc91ef95600
Nick Feng <Nick.Feng@activenetwork.com>
2023-02-08 09:17:50 +0800

    cc -std=c2x -lm -lpthread w/c/numeric/complex/Imaginary_I.c -o build/./w/c/numeric/complex/Imaginary_I.c.exe w/c/numeric/complex/Imaginary_I.c: In function ‘main’: w/c/numeric/complex/Imaginary_I.c:7:42: error: ‘_Imaginary_I’ undeclared (first use in this function)     7 |     double complex z1 = 0.0 + INFINITY * _Imaginary_I;       |                                          ^~~~~~~~~~~~ w/c/numeric/complex/Imaginary_I.c:7:42: note: each undeclared identifier is reported only once for each function it appears in

--- cppreference/w/c/numeric/complex/Imaginary_I.c
+++ cppreference/w/c/numeric/complex/Imaginary_I.c
@@ -4,9 +4,10 @@
  
 int main(void)
 {
+/*
     double complex z1 = 0.0 + INFINITY * _Imaginary_I;
     printf("z1 = %.1f%+.1fi\n", creal(z1), cimag(z1));
- 
+ */
     double complex z2 = 0.0 + INFINITY * _Complex_I;
     printf("z2 = %.1f%+.1fi\n", creal(z2), cimag(z2));
 }
